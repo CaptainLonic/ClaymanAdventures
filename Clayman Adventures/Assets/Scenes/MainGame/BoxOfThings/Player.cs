2bb5c17898dd15876d3713e8ce35462caa65686a
CaptainLonic <catboylonic@gmail.com>
2025-11-25 17:16:25 -0500

    Jomp

--- Clayman Adventures/Assets/Scenes/MainGame/BoxOfThings/Player.cs
+++ Clayman Adventures/Assets/Scenes/MainGame/BoxOfThings/Player.cs
@@ -5,7 +5,9 @@ public class Player : MonoBehaviour
     Rigidbody2D rb;
     float horizontalInput;
     float moveSpeed = 5f;
-
+    bool isFacingRight = false;
+    float jumpPower = 4f;
+    bool isJumping = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -17,10 +19,23 @@ public class Player : MonoBehaviour
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
+
+
+
+        if(Input.GetButtonDown("Jump") && !isJumping)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpPower);
+            isJumping = true;
+        }
     }
 
     private void FixedUpdate()
     {
         rb.linearVelocity = new Vector2(horizontalInput * moveSpeed, rb.linearVelocity.y);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isJumping = false;
     }
 }
